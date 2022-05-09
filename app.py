f22f183900c2c145728aa84f96ad6db78da3f631
dheerapat <dheerapat.t@kkumail.com>
2022-05-09 14:10:54 +0700

    basic flask app with form w/o db (use list instead)

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
-from flask import Flask, render_template
+from wsgiref.validate import validator
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 
-@app.route('/')
+todos = ['learn flask','activate venv','build app']
+
+class TodoForm(FlaskForm):
+    todo = StringField("Todo",validators = [DataRequired()])
+    submit = SubmitField("Add")
+
+@app.route('/', methods = ["GET","POST"])
 def index():
-    return render_template('index.html')
+    todos_form = TodoForm()
+    if todos_form.validate_on_submit():
+        new_todos = todos_form.todo.data
+        todos.append(new_todos)
+    return render_template('index.html', todos = todos, template_form = todos_form)
